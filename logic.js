336c0e7e0270acd4f2077985af43c065b4cb7fe4
Sophia Dossland <sophia.dossland@gmail.com>
2017-04-14 16:55:36 -0700

    initial commit

---
+++ logic.js
@@ -0,0 +1,22 @@
+/**
+ * Created by sophia on 4/14/17.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+
+  const books = ['A Walk to Remember', 'House on Tyneford', 'The Rescue', 'JavaScript Fundamentals', 'The Da Vinci Code'];
+
+  const list = document.querySelector('ul');
+  const listItem = list.querySelector('li');
+  const sortBtnAZ = document.querySelector('.sortAtoZ');
+  const sortBtnZA = document.querySelector('.sortZtoA');
+
+
+  function sortAZ() {
+
+  }
+
+  sortBtnAZ.addEventListener('click', sortAZ);
+  // sortBtnZA.addEventListener('click', sortZA);
+
+
+});
